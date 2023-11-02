01013b75bf142c7a9316f73a5c7ed91ff39b11ba
Bilkash <sway3942@gmail.com>
2023-11-02 17:39:51 +0200

    add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    indent: ['error', 2],
+    semi: ['error'],
+    quotes: ['error', 'single'],
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['single', 'multiple', 'none', 'all'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'linebreak-style': 'unix',
+  },
 };
